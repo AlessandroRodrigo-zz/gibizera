e993c74ee51796049e2592a5845399e998149d80
Alessandro Rodrigo <alessandro.perez@etec.sp.gov.br>
2019-05-02 21:14:29 -0300

    Criação do projeto.

---
+++ gibizera/src/java/com/dao/gibizera/TipoLombadaDAO.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao.gibizera;
+
+import com.model.gibizera.TipoLombada;
+import com.utils.gibizera.ConnectionFactory;
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ *
+ * @author alessandro
+ */
+public class TipoLombadaDAO implements GenericDAO {
+
+    private Connection conexao;
+
+    public TipoLombadaDAO() throws Exception {
+        try {
+            this.conexao = ConnectionFactory.getConnection();
+            System.out.println("Conectado com sucesso");
+        } catch (Exception e) {
+            throw new Exception("Problema ao conectar no " + "BD!" + "Erro: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Boolean cadastrar(Object objeto) {
+        TipoLombada oTipoLombada = (TipoLombada) objeto;
+        Boolean retorno = false;
+
+        if (oTipoLombada.getIdTipoPapel() == 0) {
+            retorno = this.inserir(oTipoLombada);
+        } else {
+            retorno = this.alterar(oTipoLombada);
+        }
+        return retorno;
+    }
+
+    @Override
+    public Boolean inserir(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Boolean alterar(Object objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Boolean excluir(int numero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object carregar(int numero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Object> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ gibizera/src/java/com/model/gibizera/TipoLombada.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model.gibizera;
+
+/**
+ *
+ * @author alessandro
+ */
+public class TipoLombada {
+    
+    private int idTipoPapel;
+    private String descricao;
+
+    public TipoLombada(int idTipoPapel, String descricao) {
+        this.idTipoPapel = idTipoPapel;
+        this.descricao = descricao;
+    }
+
+    public TipoLombada() {
+    }
+
+    public int getIdTipoPapel() {
+        return idTipoPapel;
+    }
+
+    public void setIdTipoPapel(int idTipoPapel) {
+        this.idTipoPapel = idTipoPapel;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
